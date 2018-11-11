5faf4bc49c4647c331384f8c521b038c6201150a
G-Cristian <registro.paginas2@gmail.com>
2018-11-11 16:00:50 -0300

    Added Color 32 bits

---
+++ Include/Color.h
@@ -0,0 +1,29 @@
+#ifndef _COLOR_H_
+#define _COLOR_H_
+
+namespace K9 {
+	typedef unsigned int Color32;
+	typedef unsigned char uchar;
+
+	inline Color32 setColor32(uchar red, uchar green, uchar blue, uchar alpha) {
+		return (((Color32)(red)) << 24) | (((Color32)(green)) << 16) | (((Color32)(blue)) << 8) | (((Color32)(alpha)));
+	}
+
+	inline uchar getRedColor32(Color32 color) {
+		return (uchar)((color & 0xFF000000) >> 24);
+	}
+
+	inline uchar getGreenColor32(Color32 color) {
+		return (uchar)((color & 0x00FF0000) >> 16);
+	}
+
+	inline uchar getBlueColor32(Color32 color) {
+		return (uchar)((color & 0x0000FF00) >> 8);
+	}
+
+	inline uchar getAlphaColor32(Color32 color) {
+		return (uchar)((color & 0x000000FF));
+	}
+}
+
+#endif // !_COLOR_H_
